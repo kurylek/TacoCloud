7af523c49725d22d54c9038b92e5ab7b3f4ece6d
Kamil Kuryłek <kurylek98@gmail.com>
2023-02-09 23:17:47 +0100

    Add action after submit design form

--- src/main/java/sia/tacos/controller/DesignTacoController.java
+++ src/main/java/sia/tacos/controller/DesignTacoController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import sia.tacos.enums.TypeOfIngredient;
@@ -42,6 +43,13 @@ public class DesignTacoController {
         model.addAttribute("design", new Taco());
 
         return "design";
+    }
+
+    @PostMapping
+    public String processDesign(Taco taco) {
+        //Logika w rodziale 3
+        log.info("Przetwarzanie projektu taco: " + taco);
+        return "redirect:/orders/current";
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, TypeOfIngredient type) {
