67589c858d236b1f24f5b9fb43966bf9b3622f92
Kamil Kuryłek <kurylek98@gmail.com>
2023-02-12 22:20:55 +0100

    Add HDB and get ingredients from database

--- src/main/java/sia/tacos/TacoCloudApplication.java
+++ src/main/java/sia/tacos/TacoCloudApplication.java
@@ -1,7 +1,12 @@
 package sia.tacos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import sia.tacos.enums.TypeOfIngredient;
+import sia.tacos.model.Ingredient;
+import sia.tacos.repository.IngredientRepository;
 
 @SpringBootApplication
 public class TacoCloudApplication {
@@ -10,4 +15,22 @@ public class TacoCloudApplication {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner dataLoader(IngredientRepository repo) {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				repo.save(new Ingredient("FLTO", "pszenna", TypeOfIngredient.WRAP));
+				repo.save(new Ingredient("COTO", "kukurydziana", TypeOfIngredient.WRAP));
+				repo.save(new Ingredient("GRBF", "mielona wołowina", TypeOfIngredient.PROTEIN));
+				repo.save(new Ingredient("CARN", "kawałki mięsa", TypeOfIngredient.PROTEIN));
+				repo.save(new Ingredient("TMTO", "pomidory pokrojone w kostkę", TypeOfIngredient.VEGGIES));
+				repo.save(new Ingredient("LETC", "sałata", TypeOfIngredient.VEGGIES));
+				repo.save(new Ingredient("CHED", "cheddar", TypeOfIngredient.CHEESE));
+				repo.save(new Ingredient("JACK", "Monterrey Jack", TypeOfIngredient.CHEESE));
+				repo.save(new Ingredient("SLSA", "salsa", TypeOfIngredient.SAUCE));
+				repo.save(new Ingredient("SRCR", "śmietana", TypeOfIngredient.SAUCE));
+			}
+		};
+	}
 }
--- src/main/java/sia/tacos/controller/DesignTacoController.java
+++ src/main/java/sia/tacos/controller/DesignTacoController.java
@@ -1,6 +1,7 @@
 package sia.tacos.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import sia.tacos.enums.TypeOfIngredient;
 import sia.tacos.model.Ingredient;
 import sia.tacos.model.Taco;
+import sia.tacos.repository.IngredientRepository;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,23 +22,15 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/design")
 public class DesignTacoController {
 
+    private final IngredientRepository ingredientRepository;
+
     @GetMapping
     public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "pszenna", TypeOfIngredient.WRAP),
-                new Ingredient("COTO", "kukurydziana", TypeOfIngredient.WRAP),
-                new Ingredient("GRBF", "mielona wołowina", TypeOfIngredient.PROTEIN),
-                new Ingredient("CARN", "kawałki mięsa", TypeOfIngredient.PROTEIN),
-                new Ingredient("TMTO", "pomidory pokrojone w kostkę", TypeOfIngredient.VEGGIES),
-                new Ingredient("LETC", "sałata", TypeOfIngredient.VEGGIES),
-                new Ingredient("CHED", "cheddar", TypeOfIngredient.CHEESE),
-                new Ingredient("JACK", "Monterey Jack", TypeOfIngredient.CHEESE),
-                new Ingredient("SLSA", "pikantny sos pomidorowy", TypeOfIngredient.SAUCE),
-                new Ingredient("SRCR", "śmietana", TypeOfIngredient.SAUCE)
-        );
+        List<Ingredient> ingredients = (List<Ingredient>) ingredientRepository.findAll();
 
         TypeOfIngredient[] typesOfIngredients = TypeOfIngredient.values();
         for (TypeOfIngredient type : typesOfIngredients) {
