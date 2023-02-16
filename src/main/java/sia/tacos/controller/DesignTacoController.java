2cdfb434320a4b9fbab78ccabc87ecff35652f99
Kamil Kuryłek <kurylek98@gmail.com>
2023-02-16 20:57:44 +0100

    Update Controllers to use repo

--- src/main/java/sia/tacos/controller/DesignTacoController.java
+++ src/main/java/sia/tacos/controller/DesignTacoController.java
@@ -1,32 +1,46 @@
 package sia.tacos.controller;
 
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import sia.tacos.enums.TypeOfIngredient;
 import sia.tacos.model.Ingredient;
+import sia.tacos.model.Order;
 import sia.tacos.model.Taco;
 import sia.tacos.repository.IngredientRepository;
+import sia.tacos.repository.TacoRepository;
 
-import java.util.Arrays;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Slf4j
 @Controller
-@RequiredArgsConstructor
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
 
     private final IngredientRepository ingredientRepository;
+    private TacoRepository designRepository;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository, TacoRepository designRepository) {
+        this.ingredientRepository = ingredientRepository;
+        this.designRepository = designRepository;
+    }
+
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
 
     @GetMapping
     public String showDesignForm(Model model) {
@@ -37,18 +51,21 @@ public class DesignTacoController {
             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
         }
 
-        model.addAttribute("design", new Taco());
+        //model.addAttribute("design", new Taco());
 
         return "design";
     }
 
     @PostMapping
-    public String processDesign(@Valid @ModelAttribute("design") Taco taco, Errors errors) {
+    public String processDesign(@Valid Taco taco, Errors errors, @ModelAttribute Order order) {
         if (errors.hasErrors()) {
             return "design";
         }
-        //Logika w rodziale 3
-        log.info("Przetwarzanie projektu taco: " + taco);
+
+        Taco createdTaco = designRepository.save(taco);
+        order.addDesign(createdTaco);
+        System.out.println(designRepository.findAll());
+
         return "redirect:/orders/current";
     }
 
--- src/main/java/sia/tacos/controller/OrderController.java
+++ src/main/java/sia/tacos/controller/OrderController.java
@@ -8,26 +8,37 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import sia.tacos.model.Order;
+import sia.tacos.repository.OrderRepository;
 
-@Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
+
+    private OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
 
     @GetMapping("/current")
     public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
-        log.info("Zamówienie zostało złożone:" + order);
+        orderRepository.save(order);
+        sessionStatus.setComplete();
+        System.out.println(orderRepository.findAll());
+
         return "redirect:/";
     }
 }
