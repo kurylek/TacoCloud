e29b08b7f4cc998af16a7d37749f780bf8cee1c8
Kamil Kuryłek <kurylek98@gmail.com>
2023-02-17 23:18:42 +0100

    Add some security config

--- src/main/java/sia/tacos/security/SecurityConfig.java
+++ src/main/java/sia/tacos/security/SecurityConfig.java
@@ -1,16 +1,31 @@
 package sia.tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new StandardPasswordEncoder("secret");
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
     }
 }
