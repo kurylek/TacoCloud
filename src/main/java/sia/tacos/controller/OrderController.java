fadd9eab427e856c5f7894297080acb00498f774
Kamil Kuryłek <kurylek98@gmail.com>
2023-02-19 02:42:43 +0100

    Add user to order

--- src/main/java/sia/tacos/controller/OrderController.java
+++ src/main/java/sia/tacos/controller/OrderController.java
@@ -1,6 +1,8 @@
 package sia.tacos.controller;
 
 import javax.validation.Valid;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import sia.tacos.model.Order;
+import sia.tacos.model.User;
 import sia.tacos.repository.OrderRepository;
 
 @Controller
@@ -29,11 +32,13 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
+        order.setUser(user);
         orderRepository.save(order);
         sessionStatus.setComplete();
 
